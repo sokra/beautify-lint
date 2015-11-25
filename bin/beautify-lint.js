fbb8adc226bb6b05340eb80d8e8428b3eab77620
Tobias Koppers <tobias.koppers@googlemail.com>
2015-11-25 21:31:50 +0100

    Initial commit

---
+++ bin/beautify-lint.js
@@ -0,0 +1,37 @@
+var forEachBeautifiedFile = require("../lib/forEachBeautifiedFile");
+var fs = require("fs");
+var path = require("path");
+var diff = require("diff");
+
+var files = process.argv.slice(2);
+
+var settings = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), ".jsbeautifyrc"), "utf-8")).js;
+
+var errors = 0;
+var ok = 0;
+
+forEachBeautifiedFile({
+	cwd: process.cwd(),
+	files: files,
+	settings: settings
+}, function(item, callback) {
+	var content = item.content;
+	var beautifiedContent = item.beautifiedContent;
+	if(content !== beautifiedContent) {
+		console.log(diff.createPatch(item.file, content, beautifiedContent));
+		console.log();
+		errors++;
+	} else {
+		ok++;
+	}
+	callback();
+}, function(err) {
+	if(err) throw err;
+	if(errors) {
+		console.log(errors + " Errors. (" + (errors + ok) + " files checked)");
+		process.exit(1); // eslint-disable-line
+	} else {
+		console.log("Fine. (" + ok + " files checked)");
+		process.exit(0); // eslint-disable-line
+	}
+});
---
+++ bin/beautify-rewrite.js
@@ -0,0 +1,35 @@
+var forEachBeautifiedFile = require("../lib/forEachBeautifiedFile");
+var fs = require("fs");
+var path = require("path");
+var diff = require("diff");
+
+var files = process.argv.slice(2);
+
+var settings = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), ".jsbeautifyrc"), "utf-8")).js;
+
+var errors = 0;
+var ok = 0;
+
+forEachBeautifiedFile({
+	cwd: process.cwd(),
+	files: files,
+	settings: settings
+}, function(item, callback) {
+	var content = item.content;
+	var beautifiedContent = item.beautifiedContent;
+	if(content !== beautifiedContent) {
+		console.log("- " + item.file);
+		errors++;
+		fs.writeFile(item.path, beautifiedContent, "utf-8", callback);
+	} else {
+		ok++;
+		callback();
+	}
+}, function(err) {
+	if(err) throw err;
+	if(errors) {
+		console.log("Done. (" + errors + " files updated, " + (errors + ok) + " files checked)");
+	} else {
+		console.log("Done. (" + ok + " files checked)");
+	}
+});
